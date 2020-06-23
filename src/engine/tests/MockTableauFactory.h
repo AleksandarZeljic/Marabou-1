05e83c43b99a102cf1e6f20e5a49d35fc7985a01
Aleksandar Zeljic <zeljic@stanford.edu>
2020-06-23 15:55:25 -0700

    Add BoundManager to Tableau and add basic shadowing calls
    Co-authored-by: anwu1219 <haozewu@stanford.edu>
    Co-authored-by: AleksandarZeljic <zeljic@stanford.edu>

--- src/engine/tests/MockTableauFactory.h
+++ src/engine/tests/MockTableauFactory.h
@@ -16,6 +16,8 @@
 #ifndef __MockTableauFactory_h__
 #define __MockTableauFactory_h__
 
+#include "BoundManager.h"
+
 #include "MockTableau.h"
 #include "T/TableauFactory.h"
 
@@ -34,8 +36,9 @@ public:
 		}
 	}
 
-	ITableau *createTableau()
+	ITableau *createTableau( BoundManager &boundManager )
 	{
+    boundManager.getSize();
 		mockTableau.mockConstructor();
 		return &mockTableau;
 	}
