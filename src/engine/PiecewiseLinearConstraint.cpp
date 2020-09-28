1778547075a6a45ff4d10708c07a83bf50117777
anwu1219 <haozewu@stanford.edu>
2020-09-28 11:54:28 -0700

    fix compilation error

--- src/engine/PiecewiseLinearConstraint.cpp
+++ src/engine/PiecewiseLinearConstraint.cpp
@@ -160,7 +160,7 @@ PhaseStatus PiecewiseLinearConstraint::nextFeasibleCase()
     return PHASE_NOT_FIXED;
 }
 
-inline unsigned PiecewiseLinearConstraint::numFeasibleCases()
+unsigned PiecewiseLinearConstraint::numFeasibleCases()
 {
     return _numCases - _infeasibleCases->size();
 }
