1c03f5a0e8b78d9b2b5afac41014240518effe06
Aleksandar Zeljic <zeljic@stanford.edu>
2022-06-15 15:42:22 -0700

    Integrate phaseFixed and isImplication semantics

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -1954,7 +1954,7 @@ bool Engine::applyAllValidConstraintCaseSplits()
 
 bool Engine::applyValidConstraintCaseSplit( PiecewiseLinearConstraint *constraint )
 {
-    if ( constraint->isActive() && constraint->phaseFixed() )
+    if ( constraint->isActive() && ( constraint->phaseFixed() || constraint->isImplication() ) )
     {
         String constraintString;
         // This is a performance issue
